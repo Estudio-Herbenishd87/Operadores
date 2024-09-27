81ad44e4798d6d26aedc1184326f660b0b8f21ac
Herbenishd87 <herbenis.hernandez632@asys.edu.co>
2024-09-26 20:57:14 -0500

    Aritmeticos

---
+++ OperadoresAritmeticos/index.js
@@ -0,0 +1,22 @@
+let resultadoSuma = 10 + 5;
+console.log(resultadoSuma);
+
+let resultadoResta = 20 - 7;
+console.log(resultadoResta);
+
+let resultadoMultiplicacion = 3 * 4;
+console.log(resultadoMultiplicacion);
+
+let resultadoDivision = 16 / 4;
+console.log(resultadoDivision);
+
+let resultadoResiduo = 10 % 3;
+console.log(resultadoResiduo);
+
+let numero = 1;
+numero++;
+console.log(numero);
+
+let otroNumero = 5;
+otroNumero--;
+console.log(otroNumero);
